fac32784bce6030f6ca4b4d252c5ee9fff2831c2
Dimitri Van Sertima <dimitri.vansertima@gmail.com>
2016-06-22 11:31:24 -0400

    Spawning Enemies at Random
    create script that spawns enemy 0 at random

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -1,54 +1,34 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic ;
-
+﻿using UnityEngine; // Required for Unity
+using System.Collections; // Required for Arrays & other Collections
+using System.Collections.Generic; // Required to use Lists or Dictionaries
 public class Main : MonoBehaviour {
-	static public Main S ;
-
-
-	public GameObject[] prefabEnemies ;
-	public float enemySpawnPerSecond = 0.5f ;
-	public float enemySpawnPadding = 1.5f ;
-
-	public bool _______________ ;
-	public float enemySpawnRate ;
-
-
-
-	void Awake () {
-
+	static public Main S;
+	public GameObject[] prefabEnemies;
+	public float enemySpawnPerSecond = 0.5f; // # Enemies/second
+	public float enemySpawnPadding = 1.5f; // Padding for position
+	public bool ________________;
+	public float enemySpawnRate; // Delay between Enemy spawns
+	void Awake() {
 		S = this;
-		//Set Utils.cambounds
-		Utils.SetCameraBounds (this.camera);
-		//0.5 enemies/second = enemySpawnRate of 2
-		enemySpawnRate = 1f / enemySpawnPerSecond;
-		//Invoke call spawnenemy() once after a 2 second delay
-		Invoke ("SpawnEnemy", enemySpawnRate);
-
+		// Set Utils.camBounds
+		Utils.SetCameraBounds(this.camera);
+		// 0.5 enemies/second = enemySpawnRate of 2
+		enemySpawnRate = 1f/enemySpawnPerSecond; // 1
+		// Invoke call SpawnEnemy() once after a 2 second delay
+		Invoke( "SpawnEnemy", enemySpawnRate ); // 2
 	}
-
-
-	void Start(){
-
-		Screen.SetResolution (630, 900, false);
-
-		GameObject scoreGO = GameObject.Find ("ScoreCounter");
-
-	}
-
-	public void SpawnEnemy(){
-		//pick a random enemy prefab to instantiate
-		int ndx = Random.Range (0,prefabEnemies.Length);
-		GameObject go = Instantiate (prefabEnemies[ndx])as GameObject ;
-		//position the Enemy above the screen with a random x positions
+	public void SpawnEnemy() {
+		// Pick a random Enemy prefab to instantiate
+		int ndx = Random.Range(0, prefabEnemies.Length);
+		GameObject go = Instantiate( prefabEnemies[ ndx ] ) as GameObject;
+		// Position the Enemy above the screen with a random x position
 		Vector3 pos = Vector3.zero;
-		float xMin = Utils.camBounds.min.x + enemySpawnPadding;
-		float xMax = Utils.camBounds.max.x - enemySpawnPadding;
-		pos.x = Random.Range (xMin, xMax);
+		float xMin = Utils.camBounds.min.x+enemySpawnPadding;
+		float xMax = Utils.camBounds.max.x-enemySpawnPadding;
+		pos.x = Random.Range( xMin, xMax );
 		pos.y = Utils.camBounds.max.y + enemySpawnPadding;
 		go.transform.position = pos;
-		//call spawnEnemy() again in a couple of seconds
-		Invoke ("SpawnEnemy", enemySpawnRate);
+		// Call SpawnEnemy() again in a couple of seconds
+		Invoke( "SpawnEnemy", enemySpawnRate ); // 3
 	}
-
 }
