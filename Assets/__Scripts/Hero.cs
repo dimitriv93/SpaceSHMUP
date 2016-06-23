db732d3518b68635a1d39dcd2e693ba6a60a7bc8
Dimitri Van Sertima <dimitri.vansertima@gmail.com>
2016-06-23 00:27:47 -0400

    Setting Tags, Layers, and Physics
    setting tags and layers to proper objects

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -47,4 +47,17 @@ public class Hero : MonoBehaviour {
 		// rotate the ship to make it feel more dynamic
 		transform.rotation =Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
 	}
+
+	void OnTriggerEnter(Collider other) {
+		// Find the tag of other.gameObject or its parent GameObjects
+		GameObject go = Utils.FindTaggedParent(other.gameObject);
+		// If there is a parent with a tag
+		if (go != null) {
+			// Announce it
+			print("Triggered: "+go.name);
+		} else {
+			// Otherwise announce the original other.gameObject
+			print("Triggered: "+other.gameObject.name); // Move this line here!
+		}
+	}
 }
